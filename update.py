2fcdb0211b714f40f87f2f01d5bb7d47729b14da
Coleman Jenkins <coleman.yankees@gmail.com>
2021-09-10 19:32:31 -0400

    Move chores to new bot and update messages

--- update.py
+++ update.py
@@ -14,8 +14,10 @@ def log(msg):
 
 
 test_bot = "7708f26577f448fd1271f9727d"
-production_bot = "7708f26577f448fd1271f9727d"
-# production_bot = "b623f516a520ffc3ca3a5aedec"
+reminders_bot = "7708f26577f448fd1271f9727d"
+chores_bot = "7708f26577f448fd1271f9727d"
+# chores_bot = "9405772b208b778b444749aa58"
+# reminders_bot = "b623f516a520ffc3ca3a5aedec"
 
 
 def sendGroupMeMessage(msg, bot):
@@ -50,16 +52,19 @@ log("Sent test message")
 
 # Remind on Saturday
 if datetime.datetime.today().weekday() == 5:  # 5
-    content = "Remember to do your chores this weekend!\n\n"
-    content += getRandomBibleVerse()
-    sendGroupMeMessage(content, production_bot)
+    content = "Remember to do your chores this weekend!"
+    # content = "Remember to do your chores this weekend!\n\n"
+    # content += getRandomBibleVerse()
+    sendGroupMeMessage(content, reminders_bot)
     log("Sent chore reminder")
 
 # Remind on Friday
 if datetime.datetime.today().weekday() == 4:  # 4
-    content = "Remember to settle up on Splitwise if you aren't already!\n\n"
-    content += getRandomBibleVerse()
-    sendGroupMeMessage(content, production_bot)
+    content = "Remember to settle up on Splitwise if you aren't already!"
+    content += "\n-> Like this if you're settled up <-"
+    # content = "Remember to settle up on Splitwise if you aren't already!\n\n"
+    # content += getRandomBibleVerse()
+    sendGroupMeMessage(content, reminders_bot)
     log("Send Splitwise reminder")
 
 
@@ -92,10 +97,15 @@ with open(fileLocation) as csv_file:
 
 if send_chore_details:
     log("Sending chores message")
-    content += "\nLike the message if you did your chore.\nHonor the Lord with your life and find joy in Him this week :)\n - Your favorite bot\n\n"
-    content += getRandomBibleVerse()
-    sendGroupMeMessage(content, production_bot)
+    content += "\nSend a picture and like the message if you did your chore!"
+    sendGroupMeMessage(content, chores_bot)
     log("Message sent!")
+    log("Sending encouragement")
+    msg = "Honor the Lord with your life and find joy in Him this week :)\n"
+    msg += "Love you guys.\n\n"
+    msg += getRandomBibleVerse()
+    sendGroupMeMessage(msg, chores_bot)
+    log("Encouragement sent!")
 
 log("Done!")
 print()
